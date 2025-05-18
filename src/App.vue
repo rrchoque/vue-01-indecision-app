44a986cf4bbe13a11f8538f7451b6189928e1c6b
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2025-05-18 06:41:17 -0400

    Mi primer contador

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Mi primera app</h1>
+  <hr />
+  <MyCounter :value="5" />
+</template>
+
+<script setup lang="ts">
+import MyCounter from './components/MyCounter.vue'
+</script>
---
+++ src/components/MyCounter.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <h3>Counter: {{ counter }}</h3>
+    <h3>Square: {{ squareCounter }}</h3>
+    <div>
+      <button @click="increment">Incrementar</button>
+      <button @click="decrement">Decrementar</button>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+// Forma 1
+// const props = defineProps({
+//   value: {
+//     type: Number,
+//     required: true,
+//   },
+// })
+
+// Forma 2
+//const props = defineProps<{ value: number }>()
+
+interface Props {
+  value?: number
+}
+const props = defineProps<Props>()
+
+const counter = ref(props.value || 0)
+
+const increment = () => {
+  counter.value++
+}
+const decrement = () => {
+  counter.value--
+}
+const squareCounter = computed(() => {
+  return counter.value * counter.value
+})
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
