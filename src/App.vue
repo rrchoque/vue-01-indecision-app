506d6f7a29d2d92f41decccabeeba74916e650e7
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2025-05-31 07:21:21 -0400

    Se inició módulo de chat

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Mi primera App!</h1>
-  <hr />
-  <MyCounter :value="5" title="Mi contador" />
-  <MyCounterScript :value="10" title="Mi contador" />
-  <MyCounterScript2 :value="15" title="Mi contador" />
+  <IndecisionView />
 </template>
 
 <script setup lang="ts">
-import MyCounter from './components/MyCounter.vue'
-import MyCounterScript from './components/MyCounterScript.vue'
-import MyCounterScript2 from './components/my-counter-script-2/MyCounterScript2.vue'
+import IndecisionView from '@/views/indecisionView.vue'
 </script>
