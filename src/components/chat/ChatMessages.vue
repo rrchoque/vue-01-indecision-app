d92f4c16339382411f2ca0a84d3c8b2a98e7ba2a
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2025-06-03 06:30:39 -0400

    Se divió el chat en componentes

---
+++ src/components/chat/ChatMessages.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex-1 overflow-y-auto p-4">
+    <div class="flex flex-col space-y-2">
+      <!-- Messages go here -->
+      <ChatBubble :its-mine="true" message="Hola Mundo" />
+
+      <ChatBubble
+        :its-mine="false"
+        message="Sunt ex minim amet dolor officia irure."
+        image="https://yesno.wtf/assets/yes/13-c3082a998e7758be8e582276f35d1336.gif"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ChatBubble from './ChatBubble.vue'
+</script>
